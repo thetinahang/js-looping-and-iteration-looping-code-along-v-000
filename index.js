18ae2862a6a78ca8db6499eb5246172df631661a
Tina Hang <mstinahang@gmail.com>
2020-01-04 18:08:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function writeCards(name) {
     console.log(`Thank you, ${name}, for the wonderful surprise gift!`)
 }
 
-function countDown() {
-
+function countDown(number) {
+    for (let counter = number; counter > 0; counter--) {
+      console.log(`${number}`)
+    }
 }
