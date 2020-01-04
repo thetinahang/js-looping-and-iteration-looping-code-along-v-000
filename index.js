98c0b8d458e505dd991a07e1fede8be6d1897833
Tina Hang <mstinahang@gmail.com>
2020-01-04 18:25:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arrayOfNames, type) {
+  let newArray = [];
+  for (let counter = 0; counter < arrayOfNames.length; counter++) {
+    newArray.push(`Thank you, ${arrayOfNames[counter]}, for the wonderful ${type} gift!`);
+  }
+  return newArray;
+}
+
+function countDown(counter){
+   while (counter > 0){
+     console.log(counter);
+     counter--
+   }
+     console.log(0);
+ }
